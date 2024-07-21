c4e6cc9994dfac7268cab8b682458c12b94bb9af
Gor Volkov <gorvolkino@yandex.ru>
2024-07-21 18:45:04 +0300

    api file added, search film by title function realised

--- api/api.py
+++ api/api.py
@@ -1,16 +1,17 @@
 import requests
+import json
 from typing import List, Dict
+from config_data.config import API_KEY, API_BASE_URL
 
-from config_data.config import API_KEY
-
+headers = {"X-API-KEY": API_KEY}
 
 # def api_request(endpoint: str, params={}) -> requests.Response:
 #     params['key'] = API_KEY
-#     return requests.get(
-#         f'{API_BASE_URL}/{endpoint}',
-#         params=params
-#     )
-#
+#     # return requests.get(
+#     #     f'{API_BASE_URL}/{endpoint}',
+#     #     params=params
+#     # )
+#     return
 #
 # def get_langs() -> List[str]:
 #     response = api_request('getLangs');
@@ -27,11 +28,49 @@ from config_data.config import API_KEY
 #     return response.json().get('def', {})
 
 
-# ФОРМАТ ВЫВОДА РЕЗУЛЬТАТОВ
-# название,
-# описание,
-# рейтинг,
-# год производства,
-# жанр,
-# возрастной рейтинг,
-# постер
+def get_movie_by_title(title: str) -> dict:
+    params = {
+        'query': title
+    }
+    response = requests.get(
+        'https://api.kinopoisk.dev/v1.4/movie/search',
+        headers=headers,
+        params=params
+    )
+    return response.json()
+
+
+def format_movie_data(movie_data: dict) -> str:
+    title = movie_data['docs'][0]['name']
+    title_orig = movie_data['docs'][0]['alternativeName']
+    year = movie_data['docs'][0]['year']
+
+    genre_list = []
+    genres = [{'name': 'фантастика'}, {'name': 'фэнтези'}, {'name': 'боевик'}, {'name': 'приключения'}]
+    for genre in genres:
+        genre_list.append(genre['name'])
+    genre_str = ', '.join(genre_list)
+
+    genres = movie_data['docs'][0]['genres']
+    description = movie_data['docs'][0]['description']
+    rating = movie_data['docs'][0]['description']
+    age_rating = 'null'
+
+    text = (f"Название: {title} ({title_orig})\n"
+            f"Описание: {description}"
+            f"Рейтинг: {rating}\n"
+            f"Год производства: {year}\n"
+            f"Жанр: {genre_str}\n"
+            f"Возрастной рейтинг: {age_rating}\n"
+            f"Постер к фильму:\n")
+
+    return text
+
+
+user_movie_title = input("Введите название фильма: ")
+
+user_movie_data = get_movie_by_title(user_movie_title)
+text_to_user = format_movie_data(user_movie_data)
+
+print(user_movie_data)
+print(text_to_user)
--- config_data/config.py
+++ config_data/config.py
@@ -8,6 +8,7 @@ else:
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_KEY = os.getenv("API_KEY")
+API_BASE_URL = "https://api.kinopoisk.dev/"
 DEFAULT_COMMANDS = (
     ("start", "Запустить бота"),
     ("help", "Вывести справку"),
