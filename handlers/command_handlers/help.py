8ec21517e71cf533d85efa347595a3e526fc217d
Gor Volkov <steppenwolfff@mail.ru>
2024-11-30 14:20:52 +0300

    Удалена за ненадобностью команда "help"

--- handlers/command_handlers/help.py
+++
@@ -1,8 +0,0 @@
-from telebot.types import Message
-from loader import bot
-
-
-@bot.message_handler(commands=["help"])
-def bot_help(message: Message):
-    """Обработчик команды /help"""
-    bot.send_message(message.from_user.id, "Я не уверен, нужна ли тут эта команда")
